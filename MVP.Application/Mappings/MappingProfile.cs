6ff258f84bf7d11633a7e64938fe6c26d9c14a13
Alonsoprogamer <jesusalodominguez.jadp@gmail.com>
2026-03-11 07:51:35 -0600

    fix: Todos los mappings de records están corregidos. - Había un problema en cascada: todos los DTOs son record posicionales y el MappingProfile anterior los mapeaba con ForMember(Ignore), que funcionaba por casualidad en versiones anteriores de AutoMapper pero ya no en v16+ - Se hizo ConstructUsing explícito en cada record

--- MVP.Application/Mappings/MappingProfile.cs
+++ MVP.Application/Mappings/MappingProfile.cs
@@ -1,35 +1,68 @@
 using AutoMapper;
-using MVP.Domain.Entities;
 using MVP.Application.DTOs;
+using MVP.Domain.Entities;
+using System;
+using System.Collections.Generic;
 
 public class MappingProfile : Profile
 {
     public MappingProfile()
     {
+
         CreateMap<Usuario, UsuarioDTO>()
-            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Uid))
-            .ForMember(dest => dest.Roles, opt => opt.Ignore());
-            
+            .ConstructUsing((src, _) => new UsuarioDTO(
+                src.Uid,
+                src.Email ?? string.Empty,
+                src.Nombre ?? string.Empty,
+                src.PrimerApellido ?? string.Empty,
+                src.SegundoApellido,
+                null,                
+                src.TenantId,
+                new List<RolDTO>(),  
+                src.NombreCompleto
+            ))
+            .ForAllMembers(opt => opt.Ignore());
+
         CreateMap<UsuarioDTO, Usuario>()
             .ForMember(dest => dest.Id, opt => opt.Ignore())
             .ForMember(dest => dest.Uid, opt => opt.MapFrom(src => src.Id))
             .ForMember(dest => dest.TenantId, opt => opt.MapFrom(src => src.TenantId));
 
         CreateMap<Rol, RolDTO>()
-            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Uid));
+            .ConstructUsing((src, _) => new RolDTO(
+                src.Uid,
+                src.Name ?? string.Empty,
+                src.Descripcion,
+                src.TenantId
+            ))
+            .ForAllMembers(opt => opt.Ignore());
 
         CreateMap<RolDTO, Rol>()
             .ForMember(dest => dest.Id, opt => opt.Ignore())
             .ForMember(dest => dest.Uid, opt => opt.MapFrom(src => src.Id));
 
         CreateMap<Modulo, ModuloDTO>()
-            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Uid))
-            .ForMember(dest => dest.PadreId, opt => opt.MapFrom(src => src.Padre != null ? src.Padre.Uid : (Guid?)null))
-            .ForMember(dest => dest.SubModulos, opt => opt.MapFrom(src => src.SubModulos));
+            .ConstructUsing((src, ctx) => new ModuloDTO(
+                src.Uid,
+                src.Descripcion,
+                src.Icono,
+                src.Accion,          
+                src.Orden ?? 0,
+                src.Padre != null ? src.Padre.Uid : (Guid?)null,
+                ctx.Mapper.Map<List<ModuloDTO>>(src.SubModulos)
+            ))
+            .ForAllMembers(opt => opt.Ignore());
 
         CreateMap<Tenant, TenantDTO>()
-            .ForMember(dest => dest.Id, opt => opt.MapFrom(src => src.Uid));
-            
+            .ConstructUsing((src, _) => new TenantDTO(
+                src.Uid,
+                src.Nombre ?? string.Empty,
+                src.Dominio,
+                src.Borrado,
+                src.FechaCreacion
+            ))
+            .ForAllMembers(opt => opt.Ignore());
+
         CreateMap<TenantDTO, Tenant>()
             .ForMember(dest => dest.Id, opt => opt.Ignore())
             .ForMember(dest => dest.Uid, opt => opt.MapFrom(src => src.Id));

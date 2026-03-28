9ae370d3ff086c5ff04e1266d4d88c9b4b04c3cd
Jadps <jadp.xs@gmail.com>
2026-03-28 00:27:47 -0600

    fix: SetLicense

--- MVP.WebAPI/Program.cs
+++ MVP.WebAPI/Program.cs
@@ -118,7 +118,6 @@ builder.Services.AddInfrastructureServices(builder.Configuration);
 
 builder.Services.AddMassTransit(x =>
 {
-    x.SetLicense("OpenSource");
     x.AddConsumer<ReportConsumer>();
 
     x.UsingRabbitMq((context, cfg) =>
